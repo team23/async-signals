792a3097f3d7dee6a5f6107ac6ea09a370e53431
David Danier <danier@team23.de>
2022-09-08 17:53:31 +0200

    Initial version of code

---
+++ async_signals/utils.py
@@ -0,0 +1,25 @@
+import functools
+import inspect
+
+# Those functions are copied from
+# https://github.com/django/django/blob/main/django/utils/inspect.py
+
+
+@functools.lru_cache(maxsize=512)
+def _get_func_parameters(func, remove_first):
+    parameters = tuple(inspect.signature(func).parameters.values())
+    if remove_first:
+        parameters = parameters[1:]
+    return parameters
+
+
+def _get_callable_parameters(meth_or_func):
+    is_method = inspect.ismethod(meth_or_func)
+    func = meth_or_func.__func__ if is_method else meth_or_func
+    return _get_func_parameters(func, remove_first=is_method)
+
+
+def func_accepts_kwargs(func):
+    """Return True if function 'func' accepts keyword arguments **kwargs."""
+
+    return any(p for p in _get_callable_parameters(func) if p.kind == p.VAR_KEYWORD)
